958e658e76c81b21c7af2ae0e12a818a11a3b566
Cielpy <beijiu572@gmail.com>
2016-07-17 01:51:26 +0800

    add get markdown support

---
+++ get_image_url.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2016 ciel <beijiu572@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+得到图片的 URL
+"""
+
+from upload import upload_file
+import util
+import os
+
+url = upload_file()
+if url:
+    os.system("echo '%s' | pbcopy" % url)
+    util.alert('上传图片成功，图片 URL 已复制到剪切板！')
+else:
+    util.alrt('上传失败!')
---
+++ get_markdown.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2016 ciel <beijiu572@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+得到图片的 Markdown
+"""
+
+from upload import upload_file
+import util
+import os
+
+url = upload_file()
+if url:
+    markdown_url = '![](%s)' % (url)
+    os.system("echo '%s' | pbcopy" % markdown_url)
+    util.alert('上传图片成功，图片 Markdown 已复制到剪切板！')
+else:
+    util.alrt('上传失败!')
